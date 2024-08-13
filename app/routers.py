218c401533a90970399657c8d4788f6ad15c0177
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-13 16:50:31 +0300

    add routers

--- app/routers.py
+++ app/routers.py
@@ -1,8 +1,23 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from app.services import get_weater_today
 
 weater_router = APIRouter()
 
+templates = Jinja2Templates(directory="app/templates")
 
-@weater_router.get("/", status_code=status.HTTP_200_OK)
-async def get_index():
-    return {"message": "Hello, sweety"}
+
+@weater_router.get("", status_code=status.HTTP_200_OK)
+async def get_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@weater_router.post(
+    "/weater", status_code=status.HTTP_200_OK, response_class=HTMLResponse
+)
+async def get_weater(request: Request, city: str = Form(...)):
+    data = await get_weater_today(city)
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "weather": data, "city": city}
+    )
