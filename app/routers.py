e74334fd795e49c96ddc390aa1acfa90b273b32b
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-15 15:34:38 +0300

    add exception handler

--- app/routers.py
+++ app/routers.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, status, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from app.exeptions import NotFoundException, UnprocessableEntityException
 from app.services import get_weater_today
 
 weater_router = APIRouter()
@@ -17,7 +19,12 @@ async def get_index(request: Request):
     "/weater", status_code=status.HTTP_200_OK, response_class=HTMLResponse
 )
 async def get_weater(request: Request, city: str = Form(...)):
-    data = await get_weater_today(city)
-    return templates.TemplateResponse(
-        "index.html", {"request": request, "weather": data, "city": city}
-    )
+    try:
+        data = await get_weater_today(city)
+        return templates.TemplateResponse(
+            "index.html", {"request": request, "weather": data, "city": city}
+        )
+    except (NotFoundException, UnprocessableEntityException) as e:
+        return templates.TemplateResponse(
+            "index.html", {"request": request, "error": e}
+        )
