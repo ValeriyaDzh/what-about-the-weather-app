d5583acca73fc16f1476516e63ab5cbe579f0af9
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-15 15:10:19 +0300

    add repr

--- app/exeptions.py
+++ app/exeptions.py
@@ -7,9 +7,15 @@ class NotFoundException(HTTPException):
     def __init__(self, detail: str = "Not found"):
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
 
+    def __repr__(self) -> str:
+        return self.detail
+
 
 class UnprocessableEntityException(HTTPException):
     def __init__(self, detail: str = "Unprocessable entity"):
         super().__init__(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail
         )
+
+    def __repr__(self) -> str:
+        return self.detail
