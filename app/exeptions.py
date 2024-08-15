6aa8095f18292d0cec133026b00a21f3a2ad0e0e
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-15 13:57:58 +0300

    add UnprocessableEntityException

--- app/exeptions.py
+++ app/exeptions.py
@@ -1,6 +1,18 @@
+from typing import Any, Dict
+from typing_extensions import Annotated, Doc
 from fastapi import HTTPException, status
 
 
 class NotFoundException(HTTPException):
     def __init__(self, detail: str = "Not found"):
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
+
+class UnprocessableEntityException(HTTPException):
+    def __init__(self, detail: str = "Unprocessible entity"):
+        super().__init__(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail
+        )
+
+
+a = status.HTTP_422_UNPROCESSABLE_ENTITY
